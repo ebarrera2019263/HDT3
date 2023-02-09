26ce10110db58d80fea9f27e8661fda1c6969458
ebarrera2019263 <ebarrera-2019263@kinal.edu.gt>
2023-02-08 20:26:20 -0600

    se agregaron la clase principal

--- Principal.java
+++ Principal.java
@@ -1,3 +1,164 @@
+
+/* Integrantes: Erick Barrera 22934
+ *              Jorge Lopez 221038
+ * 
+ * 
+*/
+
 public class Principal {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        Read read = new Read();
+        Integer[] numeros = read.VerificarDocumento();
+        Aplicacion_Sort aSort = new Aplicacion_Sort(new Controladora());
+
+        String menu = "\n1. Gnome Sort. \n2. Merge Sort. \n3. Quick Sort. \n4. Radix Sort.  \n5. Bubble Sort \n6. Salir \nIndica lo que quieres realizar aquí: ";
+        System.out.println("\n\t\t\t\t\t***Bienvenido a los diferentes Ordenamientos Sorts ***");
+        int opcion = 1;
+
+        while(opcion >= 1 && opcion < 6){
+            System.out.print(menu);
+            try{
+                opcion = teclado.nextInt();
+                teclado.nextLine();
+                switch(opcion){
+                    
+                    /*
+                     * Gnome Sort
+                     */
+                    case 1:{
+                        System.out.println("Gnome Sort");
+
+                        for(int i = 10; i <= 5000; i+=499){
+                            Integer[] lista = new Integer[i];
+            			
+                            for(int j=0; j<i; j++) {
+                                lista[j]=numeros[j];
+                            }                           
+                            aSort.GnomeSort(lista);
+                            
+                            for(int recorrido:lista) {
+                                System.out.println(recorrido);
+                            }
+                            System.out.println("\nOrdenamiento con: " + i + " numeros.\n");
+                            System.out.println("Presionar Enter para seguir");
+                            teclado.nextLine();
+                        }
+
+                        System.out.println("Lista Ordenada");
+                        break;                   
+                    }
+
+                    /*
+                     * Merge Sort
+                     */
+                    case 2:{
+                        for(int i = 10; i <= 5000; i+=499){
+                            Integer[] lista = new Integer[i];
+            			
+                            for(int j=0; j<i; j++) {
+                                lista[j]=numeros[j];
+                            }                           
+                            aSort.MergeSort(lista, 0, lista.length-1);
+                            
+                            for(int recorrido:lista) {
+                                System.out.println(recorrido);
+                            }
+                            System.out.println("\nOrdenamiento con: " + i + " numeros.\n");
+                            System.out.println("Presionar Enter para seguir");
+                            teclado.nextLine();
+                        }
+
+                        System.out.println("Lista Ordenada");
+                        break;    
+                    }
+    
+                    /*
+                     * Quick Sort
+                     */
+                    case 3:{
+                        for(int i = 10; i <= 5000; i+=499){
+                            Integer[] lista = new Integer[i];
+            			
+                            for(int j=0; j<i; j++) {
+                                lista[j]=numeros[j];
+                            }                           
+                            aSort.QuickSort(lista, 0, lista.length-1);
+                            
+                            for(int recorrido:lista) {
+                                System.out.println(recorrido);
+                            }
+                            System.out.println("\nOrdenamiento con: " + i + " numeros.\n");
+                            System.out.println("Presionar Enter para seguir");
+                            teclado.nextLine();
+                        }
+
+                        System.out.println("Lista Ordenada");
+                        break;
+                    }
+    
+                    /*
+                     * Radix Sort
+                     */
+                    case 4:{
+                        for(int i = 10; i <= 5000; i+=499){
+                            Integer[] lista = new Integer[i];
+            			
+                            for(int j=0; j<i; j++) {
+                                lista[j]=numeros[j];
+                            }                           
+                            aSort.RadixSort(lista);
+                            
+                            for(int recorrido:lista) {
+                                System.out.println(recorrido);
+                            }
+                            System.out.println("\nOrdenamiento con: " + i + " numeros.\n");
+                            System.out.println("Presionar Enter para seguir");
+                            teclado.nextLine();
+                        }
+
+                        System.out.println("Lista Ordenada");
+                        break;
+                    }
+
+                    /*
+                     * Bubble Sort
+                     */
+                    case 5:{
+                        for(int i = 10; i <= 5000; i+=499){
+                            Integer[] lista = new Integer[i];
+            			
+                            for(int j=0; j<i; j++) {
+                                lista[j]=numeros[j];
+                            }                           
+                            aSort.BubbleSort(lista);
+                            
+                            for(int recorrido:lista) {
+                                System.out.println(recorrido);
+                            }
+                            System.out.println("\nOrdenamiento con: " + i + " numeros.\n");
+                            System.out.println("Presionar Enter para seguir");
+                            teclado.nextLine();
+                        }
+
+                        System.out.println("Lista Ordenada");
+                        break;
+                    }
+
+                }
+            
+            //Esto es en caso de que el usuario llegue colocar un caracter que no acepta el programa o si no hay datos creados 
+            }catch(Exception e){
+                System.out.println("Has ingresado uno o varios caracteres incorrectos o no hay datos existentes, intenta nuevamente");
+                teclado.next();
+            }    
+
+        }
+
+        System.out.println("Gracias por utilizar Nuestro programa, hasta luego...");
+    }
     
 }
