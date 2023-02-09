03a3d34feebe040252a4d7cc741d0e4592daad1d
jorge6217 <106042723+jorge6217@users.noreply.github.com>
2023-02-08 20:49:55 -0600

    Se agrego la  clase controladora

--- Controladora.java
+++ Controladora.java
@@ -1,3 +1,19 @@
-public class Controladora {
+public class Controladora<T> implements ICompareTo<T> {
+
+    @Override
+    public int Compare(T objeto1, T objeto2) {
+        int numero1, numero2;
+
+        numero1 = Integer.parseInt(objeto1.toString());
+        numero2 = Integer.parseInt(objeto2.toString());
+
+        if(numero1 > numero2){
+            return 1;
+        }else if(numero1 < numero2){
+            return -1;
+        }else{
+            return 0;
+        }
+    }
     
 }
